6cd199f3d38967b7cac109f3b196e7d88d000187
pokecrystal-mobile <110418063+pokecrystal-mobile@users.noreply.github.com>
2022-08-08 16:12:51 -0400

    Update mobile_5c.asm

--- mobile/mobile_5c.asm
+++ mobile/mobile_5c.asm
@@ -811,9 +811,9 @@ INCBIN "gfx/pokedex/slowpoke_mobile.2bpp.lz"
 String_172e31:
 	db "PASSWORT-Eingabe@";"パスワード<WO>いれてください@"
 String_172e3f:
-	db " 123▶ ZUR.  OK @";"きりかえ　やめる　　けってい@"
+	db " 123▶ ZUR. O.K.@";"きりかえ　やめる　　けってい@"
 String_172e4e:
-	db " ABC▶ ZUR.  OK @";"きりかえ　やめる　　"
+	db " ABC▶ ZUR. O.K.@";"きりかえ　やめる　　"
 String_172e58:
 	db "OK@";"けってい@"
 String_172e5d:
